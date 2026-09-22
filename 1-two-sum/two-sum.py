a19be136c2c2d865899a9ec5945b2eaf2c930199
Rahul N N <nnrahul6@gmail.com>
2026-09-22 22:42:50 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (42.39%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,12 @@
+# Two Sum - Find Two Indices Whose Values Add Up to Target
+class Solution:
+    def twoSum(self, nums, target):
+        seen = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+
+            if complement in seen:
+                return [seen[complement], i]
+
+            seen[num] = i     
